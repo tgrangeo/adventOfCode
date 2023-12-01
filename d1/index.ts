12c0c37b99a97e8dbd9c1cade369016eecc7d62a
tgrangeo <thomas.grangeon9@gmail.com>
2023-12-01 22:21:14 +0100

    d1p2

--- d1/index.ts
+++ d1/index.ts
@@ -1,12 +1,34 @@
 import * as fs from 'fs';
 
 
-var str = fs.readFileSync('input.txt','utf8');
+var str = fs.readFileSync('input.txt', 'utf8');
 var array = str.split('\n')
 var sum = 0
-array.forEach((e)=> {
-	var tmp = e.replace(/[^0-9]/g,"");
-	str = String(tmp[0]).concat(tmp[tmp.length - 1])
+array.forEach((e) => {
+	var str = ""
+	for (var i = 0; i < e.length; i++) {
+		if (Number(e[i]))
+			str += e[i]
+		if (e.startsWith("one", i))
+			str += "1"
+		if (e.startsWith("two", i))
+			str += "2"
+		if (e.startsWith("three", i))
+			str += "3"
+		if (e.startsWith("four", i))
+			str += "4"
+		if (e.startsWith("five", i))
+			str += "5"
+		if (e.startsWith("six", i))
+			str += "6"
+		if (e.startsWith("seven", i))
+			str += "7"
+		if (e.startsWith("eight", i))
+			str += "8"
+		if (e.startsWith("nine", i))
+			str += "9"
+	}
+	str = String(str[0]).concat(str[str.length -1])
 	if (Number(str))
 		sum += Number(str)
 })
